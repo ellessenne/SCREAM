8876199fadd230ebbb76da0e5a9de3ac9700d5c7
Alessandro Gasparini <ellessenne@gmail.com>
2021-07-26 11:10:40 +0200

    Updated permanence tests with new name

--- tests/testthat/test-multimorbidity-permanence.R
+++ tests/testthat/test-multimorbidity-permanence.R
@@ -1,17 +1,17 @@
 ### Test that permanence is applied properly
-for (c in seq_along(.multimorbidity_permanent())) {
+for (c in seq_along(.multimorbidity_years())) {
   .loc <- .multimorbidity_codes()[[c]]
   .condition <- names(.multimorbidity_codes())[c]
   for (i in seq_along(.loc)) {
     # First, codes are removed
     index_date <- sample(x = seq(as.Date("2000-01-01"), as.Date("2020-12-31"), by = 1), size = 1)
-    if (isTRUE(.multimorbidity_permanent()[[c]])) {
+    if (is.null(.multimorbidity_years()[[c]])) {
       tmp <- data.frame(id = 1, code = .loc[i], index_date = index_date, date = index_date - 1)
     } else {
-      tmp <- data.frame(id = 1, code = .loc[i], index_date = index_date, date = index_date - 400 * .multimorbidity_permanent()[[c]])
+      tmp <- data.frame(id = 1, code = .loc[i], index_date = index_date, date = index_date - 400 * .multimorbidity_years()[[c]])
     }
     out <- multimorbidity(data = tmp, id = "id", code = "code", date = "date", index_date = "index_date")
-    if (isTRUE(.multimorbidity_permanent()[[c]])) {
+    if (is.null(.multimorbidity_years()[[c]])) {
       testthat::expect_equal(object = out[[.condition]], expected = 1)
     } else {
       testthat::expect_equal(object = out[[.condition]], expected = 0)
