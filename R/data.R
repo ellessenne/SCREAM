c355b81bf0e06c1c27b46a735f15233faf7db7e6
Alessandro Gasparini <ellessenne@gmail.com>
2021-07-30 16:57:33 +0300

    Updated bundled ICD-10 data

--- R/data.R
+++ R/data.R
@@ -1,13 +1,11 @@
 #' @title Example of a dataset with ICD-10 codes
 #'
-#' @format A data frame with 123,772 rows and 7 variables, for 5,000 subjects. Included variables are:
+#' @format A list with two simulated data frames of ICD-10 codes for 3,000 subjects, one with hospitalisation codes, and one with inpatient and outpatient claims.
+#' Each data set includes the following variables:
 #' * `id` Subject ID.
 #' * `date` Date corresponding to each code.
 #' * `index_date` Index date (used e.g. to calculate multimorbidity at that specific point in time).
 #' * `code` ICD-10 code.
-#' * `origin` Origin of each ICD-10 code, with `origin = 1` denoting hospital care, `origin = 2` denoting secondary care, and `origin = 3` denoting primary care.
-#' * `n` Sequential number for each observation.
-#' * `N` Total number of observations per subject.
 #' @rdname icd10
 #'
 #' @examples
--- data-raw/data-icd10.R
+++ data-raw/data-icd10.R
@@ -4,22 +4,24 @@ library(tidyverse)
 
 set.seed(329746238)
 
-.N <- 5000
-.n <- rpois(n = .N, lambda = 50)
+.N <- 3000
+.n <- rpois(n = .N, lambda = 30)
 .dates <- seq(as.Date("2010-01-01"), as.Date("2019-12-31"), by = 1)
 
 icd10 <- data.frame(
   id = rep(x = seq(.N), times = .n),
   date = sample(x = .dates, size = sum(.n), replace = TRUE),
   index_date = rep(x = sample(x = .dates, size = .N, replace = TRUE), times = .n),
-  code = comorbidity::sample_diag(n = sum(.n)),
-  origin = sample(x = seq(3), size = sum(.n), prob = c(0.2, 0.5, 0.4), replace = TRUE)
+  code = comorbidity::sample_diag(n = sum(.n))
 )
 
 icd10 <- arrange(icd10, id, date) %>%
-  filter(date <= index_date) %>%
-  group_by(id) %>%
-  mutate(n = row_number(), N = n()) %>%
-  ungroup()
+  filter(date <= index_date)
+icd10$h <- rbinom(n = nrow(icd10), size = 1, prob = 1 / 3)
+
+icd10 <- list(
+  hospitalisations = icd10 %>% filter(h == 1) %>% select(-h),
+  claims = icd10 %>% select(-h)
+)
 
 usethis::use_data(icd10, overwrite = TRUE)
