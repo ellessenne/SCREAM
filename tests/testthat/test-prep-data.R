ea036d2ba4e985a501f6288a3e2ee4433ea8c191
Alessandro Gasparini <ellessenne@gmail.com>
2021-09-23 13:34:52 +0200

    Testing that drug data is prepared correctly

--- tests/testthat/test-prep-data.R
+++ tests/testthat/test-prep-data.R
@@ -1,4 +1,4 @@
-test_that("Data is prep'd properly", {
+test_that("ICD data is prep'd properly", {
   for (i in seq(50)) {
     a <- data.frame(.tmp = 1)
     .id <- .sample_name()
@@ -13,6 +13,28 @@ test_that("Data is prep'd properly", {
     testthat::expect_equal(object = names(b), expected = c("id", "code", "date", "index_date"))
     testthat::expect_equal(object = nrow(b), expected = 1)
     testthat::expect_equal(object = ncol(b), expected = 4)
+    testthat::expect_true(object = ncol(a) > ncol(b))
+  }
+})
+
+test_that("Drugs data is prep'd properly", {
+  data("atc", package = "SCREAM")
+  for (i in seq(50)) {
+    a <- data.frame(.tmp = 1)
+    .id <- .sample_name()
+    .atc <- .sample_name()
+    .npacks <- .sample_name()
+    .date <- .sample_name()
+    .index_date <- .sample_name()
+    a[[.id]] <- 1
+    a[[.atc]] <- sample(x = atc$ATC.Code, size = 1)
+    a[[.npacks]] <- 1
+    a[[.date]] <- Sys.Date() - 1
+    a[[.index_date]] <- Sys.Date()
+    b <- .multimorbidity_prep_drugs(data = a, id = .id, atc = .atc, npacks = .npacks, date = .date, index_date = .index_date)
+    testthat::expect_equal(object = names(b), expected = c("id", "atc", "npacks", "date", "index_date"))
+    testthat::expect_equal(object = nrow(b), expected = 1)
+    testthat::expect_equal(object = ncol(b), expected = 5)
     testthat::expect_true(object = ncol(a) > ncol(b))
   }
 })
