a5875746ab10ccc8ae0473de1ae2f0a675f7e9fe
Alessandro Gasparini <ellessenne@gmail.com>
2021-09-23 13:54:00 +0200

    Fixed tests for detection of hospitalisation codes

--- tests/testthat/test-detect-hospitalisations.R
+++ tests/testthat/test-detect-hospitalisations.R
@@ -1,12 +1,13 @@
 testthat::test_that("Codes for hospitalisations are detected properly", {
+  Dn <- data.frame(id = 1, index_date = Sys.Date(), date = Sys.Date() - 1, atc = "NOTaDRUG", npacks = 1)
   Cn <- data.frame(id = 1, code = "NOTaCODE", index_date = Sys.Date(), date = Sys.Date() - 1)
   for (mi in seq_along(.multimorbidity_codes())) {
     for (c in .multimorbidity_codes()[[mi]]) {
       hm <- .multimorbidity_howmany()[[mi]]
       Hy <- data.frame(id = 1, index_date = Sys.Date(), date = Sys.Date() - seq(hm["hospitalisations"]), code = c)
       Hn <- data.frame(id = 1, index_date = Sys.Date(), date = Sys.Date() - seq(hm["hospitalisations"]), code = "NOTaCODE")
-      outy <- multimorbidity(data_hospitalisations = Hy, data_claims = Cn, id = "id", code = "code", date = "date", index_date = "index_date", combine_cirrhosis = FALSE)
-      outn <- multimorbidity(data_hospitalisations = Hn, data_claims = Cn, id = "id", code = "code", date = "date", index_date = "index_date", combine_cirrhosis = FALSE)
+      outy <- multimorbidity(data_hospitalisations = Hy, data_claims = Cn, data_drugs = Dn, id = "id", code = "code", atc = "atc", npacks = "npacks", date = "date", index_date = "index_date", combine_cirrhosis = FALSE)
+      outn <- multimorbidity(data_hospitalisations = Hn, data_claims = Cn, data_drugs = Dn, id = "id", code = "code", atc = "atc", npacks = "npacks", date = "date", index_date = "index_date", combine_cirrhosis = FALSE)
       w <- names(.multimorbidity_codes())[mi]
       testthat::expect_equal(object = outy[[w]], expected = 1)
       testthat::expect_equal(object = outy[[w]], expected = 1)
